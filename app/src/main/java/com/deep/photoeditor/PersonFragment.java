1943bb09bb329daceda4da14ff7977d9e6c786c9
vickyliao <vicky1000266@gmail.com>
2020-04-30 21:06:51 +0800

    create 2 fragment(collect and works) for personal page

--- app/src/main/java/com/deep/photoeditor/PersonFragment.java
+++ app/src/main/java/com/deep/photoeditor/PersonFragment.java
@@ -4,15 +4,59 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
-public class PersonFragment extends Fragment {
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+public class PersonFragment extends Fragment implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_person, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_person, container, false);
+
+        Button btnShowCollect = (Button) rootView.findViewById(R.id.btnCollect);
+        Button btnShowWorks = (Button) rootView.findViewById(R.id.btnWorks);
+
+        btnShowCollect.setOnClickListener(this);
+        btnShowWorks.setOnClickListener(this);
+        Fragment fragment = new PerCollectFragment();
+        replaceFragment(fragment);
+        return rootView;
     }
+
+    @Override
+    public void onClick(View view) {
+        Fragment fragment = null;
+
+        switch (view.getId()) {
+            case R.id.btnCollect:
+                fragment = new PerCollectFragment();
+                replaceFragment(fragment);
+                break;
+
+            case R.id.btnWorks:
+                fragment = new PerWorksFragment();
+                replaceFragment(fragment);
+                break;
+        }
+    }
+
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.personrelativeLayout, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+
 }
