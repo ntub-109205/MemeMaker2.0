59986852124e0a03032ea3e7d04c42267a7d16b1
Angela822 <angelayu822@gmail.com>
2020-05-03 17:12:35 +0800

    新增login和signUp

--- app/src/main/java/com/deep/photoeditor/login.java
+++ app/src/main/java/com/deep/photoeditor/login.java
@@ -1,14 +1,47 @@
-package com.burhanrashid52.photoeditor;
+package com.deep.photoeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+    //button onClick to next page
+    public Button btnSignin;
+    public Button btnToSignup;
+
+    public void init(){
+        btnSignin = (Button)findViewById(R.id.btnSignin);
+        btnToSignup = (Button)findViewById(R.id.btnToSignup);
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //new一個intent物件，並指定Activity切換的class
+                Intent edit = new Intent(login.this,MainActivity.class);
+                //切換Activity
+                startActivity(edit);
+            }
+        });
+
+        btnToSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //new一個intent物件，並指定Activity切換的class
+                Intent edit = new Intent(login.this,signUp.class);
+                //切換Activity
+                startActivity(edit);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        init();
     }
 }
--- app/src/main/java/com/deep/photoeditor/signUp.java
+++ app/src/main/java/com/deep/photoeditor/signUp.java
@@ -1,14 +1,52 @@
-package com.burhanrashid52.photoeditor;
+package com.deep.photoeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class signUp extends AppCompatActivity {
+    //button onClick to next page
+    public Button btnSignup;
+    public Button btnToLoginin;
+
+    public void init(){
+        btnSignup = (Button)findViewById(R.id.btnSignUp);
+        btnToLoginin = (Button)findViewById(R.id.btnToLoginIn);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //new一個intent物件，並指定Activity切換的class
+                Intent edit = new Intent(signUp.this,MainActivity.class);
+                //切換Activity
+                startActivity(edit);
+            }
+        });
+
+        btnToLoginin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //new一個intent物件，並指定Activity切換的class
+                Intent edit = new Intent(signUp.this,login.class);
+                //切換Activity
+                startActivity(edit);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //新增回到前一頁的箭頭
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        init();
+
     }
 }
