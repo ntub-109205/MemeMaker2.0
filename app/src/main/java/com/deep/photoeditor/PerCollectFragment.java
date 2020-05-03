06119ccbf0044765f8abf93cb634eed2a1f7a5a7
vickyliao <vicky1000266@gmail.com>
2020-05-03 18:28:29 +0800

    update person layout 2

--- app/src/main/java/com/deep/photoeditor/PerCollectFragment.java
+++ app/src/main/java/com/deep/photoeditor/PerCollectFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-public class PerCollectFragment extends Fragment {
+public class PerCollectFragment extends Fragment implements View.OnClickListener {
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_per_collect, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_per_collect, container, false);
+        return rootView;
     }
 
-    
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
