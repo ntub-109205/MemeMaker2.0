8052e60ebce85bb303ab6241dc4df7ffc7832884
vickyliao <vicky1000266@gmail.com>
2020-05-12 01:27:49 +0800

    update personal page: add upper background and change button style

--- app/src/main/java/com/deep/photoeditor/PersonFragment.java
+++ app/src/main/java/com/deep/photoeditor/PersonFragment.java
@@ -1,6 +1,7 @@
 package com.deep.photoeditor;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,14 +9,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class PersonFragment extends Fragment implements View.OnClickListener {
+    View rootView;
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_person, container, false);
+        rootView = inflater.inflate(R.layout.fragment_person, container, false);
 
         Button btnShowCollect = (Button) rootView.findViewById(R.id.btnCollect);
         Button btnShowWorks = (Button) rootView.findViewById(R.id.btnWorks);
@@ -25,22 +29,29 @@ public class PersonFragment extends Fragment implements View.OnClickListener {
 
         Fragment fragment = new PerCollectFragment();
         replaceFragment(fragment);
+        btnShowCollect.setBackground(getResources().getDrawable(R.drawable.button_press));
         return rootView;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onClick(View view) {
         Fragment fragment = null;
-
+        Button btnShowCollect = (Button) rootView.findViewById(R.id.btnCollect);
+        Button btnShowWorks = (Button) rootView.findViewById(R.id.btnWorks);
         switch (view.getId()) {
             case R.id.btnCollect:
                 fragment = new PerCollectFragment();
                 replaceFragment(fragment);
+                btnShowCollect.setBackground(getResources().getDrawable(R.drawable.button_press));
+                btnShowWorks.setBackground(getResources().getDrawable(R.drawable.button_default));
                 break;
 
             case R.id.btnWorks:
                 fragment = new PerWorksFragment();
                 replaceFragment(fragment);
+                btnShowCollect.setBackground(getResources().getDrawable(R.drawable.button_default));
+                btnShowWorks.setBackground(getResources().getDrawable(R.drawable.button_press));
                 break;
         }
     }
