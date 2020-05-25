ef17e3e790c74534d1de2d6d73b6345680ccbeaf
vickyliao <vicky1000266@gmail.com>
2020-05-25 23:26:34 +0800

    update person collect and work page: all pages can jump now!

---
+++ app/src/main/java/com/deep/photoeditor/fragment/ColEldFragment.java
@@ -0,0 +1,66 @@
+package com.deep.photoeditor.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import com.deep.photoeditor.PublicMeme;
+import com.deep.photoeditor.R;
+import com.deep.photoeditor.adpater.RecyclerViewAdapter_colEld;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ColEldFragment extends Fragment {
+
+    View v;
+    private RecyclerView myrecyclerview;
+    private List<PublicMeme> lstMemeMeme;
+
+
+    public ColEldFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_col_elder, container, false);
+        myrecyclerview = (RecyclerView) v.findViewById(R.id.col_eld_recyclerView);
+        RecyclerViewAdapter_colEld recyclerViewAdapter = new RecyclerViewAdapter_colEld(getContext(),lstMemeMeme);
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL);
+        myrecyclerview.setLayoutManager(staggeredGridLayoutManager);
+        myrecyclerview.setAdapter(recyclerViewAdapter);
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        lstMemeMeme = new ArrayList<>();
+        lstMemeMeme.add(new PublicMeme("#Dog #隔離","https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1585196981888.jpg","潔西卡",35));
+        lstMemeMeme.add(new PublicMeme("#反叛的魯路修","https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1589009676654.jpg","Geo",20));
+        lstMemeMeme.add(new PublicMeme("#三國殺","https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1589044855818.jpg","跑跑跑的人",19));
+        lstMemeMeme.add(new PublicMeme("#鼠定了你","https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1589086291871.jpg","好累",18));
+        lstMemeMeme.add(new PublicMeme("#某系列主機遊戲","https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1589041392638.jpg","Anc1233",17));
+        lstMemeMeme.add(new PublicMeme("#PM日常","https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1589041053097.jpg","牙醫09",16));
+        lstMemeMeme.add(new PublicMeme("#歷史","https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1589034990886.jpg","江戶川先生",14));
+
+
+
+    }
+}
