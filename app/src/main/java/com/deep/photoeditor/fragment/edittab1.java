b9952dcbaac0a243ee90c882b8b96f03cea04b9a
Angela822 <angelayu822@gmail.com>
2020-06-05 00:47:03 +0800

    update comment

--- app/src/main/java/com/deep/photoeditor/fragment/edittab1.java
+++ app/src/main/java/com/deep/photoeditor/fragment/edittab1.java
@@ -89,14 +89,5 @@ public class edittab1 extends Fragment {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-//        lsthotTemplate = new ArrayList<>();
-//        lsthotTemplate.add(new hotTemplate("Idot Dog","https://img.ltn.com.tw/Upload/partner/page/2019/03/30/190330-3313-1-phpUCF6ub.jpg","安潔拉",20));
-//        lsthotTemplate.add(new hotTemplate("cute dog","https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.180.1440.1440a/s640x640/95312078_952632711849290_8581521202673882401_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=grzPhbZX09oAX_DwdUd&oh=2ca5328cbd03458942253918a5843ef6&oe=5ED6ED2E","Vicky",20));
-//        lsthotTemplate.add(new hotTemplate("cat","https://imgs.yamedia.tw/yamsales1966/20191009/9ff4fb0d-fa20-4086-9aea-9caf7ff973cb.jpg?h=250","Zark",25));
-//        lsthotTemplate.add(new hotTemplate("Idot star","https://attach.setn.com/newsimages/2020/03/11/2450111-XXL.jpg","Taro",33));
-//        lsthotTemplate.add(new hotTemplate("morning flower","https://3.bp.blogspot.com/-XWxh1r3GMPg/VwFh6DJfBqI/AAAAAAABV-Y/VGKLtGmC_hkN32eK-KjhpDeFo7jIwq1ZA/s1600/flowering%2BQuince%2B3.jpg","Jessie",25));
-//        lsthotTemplate.add(new hotTemplate("my idol","https://www.wepeople.club/new-wepeople-upload/bb3886dc4b408776b51b1ba18ad3fb1e.jpg","Angela",15));
-
     }
 }
