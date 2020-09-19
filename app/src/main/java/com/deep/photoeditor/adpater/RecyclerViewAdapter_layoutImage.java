811a793fb141ca2eee368da052c9d14b3b0c9501
Angela822 <angelayu822@gmail.com>
2020-09-19 12:07:08 +0800

    [RecyclerViewAdapter_layoutImage] add/remove selectorView | Angelayu

--- app/src/main/java/com/deep/photoeditor/adpater/RecyclerViewAdapter_layoutImage.java
+++ app/src/main/java/com/deep/photoeditor/adpater/RecyclerViewAdapter_layoutImage.java
@@ -2,86 +2,86 @@ package com.deep.photoeditor.adpater;
 
 import android.content.Context;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.deep.photoeditor.R;
 import com.deep.photoeditor.layoutImage;
-
+import com.deep.photoeditor.R;
+import java.util.HashMap;
 import java.util.List;
 
-public class RecyclerViewAdapter_layoutImage extends RecyclerView.Adapter<RecyclerViewAdapter_layoutImage.MyViewHolder> {
-    Context mContext;
-    List<layoutImage> mData;
+public class RecyclerViewAdapter_layoutImage extends RecyclerView.Adapter<RecyclerViewAdapter_layoutImage.ImageViewHolder> {
+    private Context mContext;
+    private List<layoutImage> mLayoutImageList;
+    private HashMap<View, ImageView> mImageViewMap = new HashMap<View, ImageView>();
 
-    public RecyclerViewAdapter_layoutImage(Context mContext, List<layoutImage> mData) {
-        this.mContext = mContext;
-        this.mData = mData;
+    public RecyclerViewAdapter_layoutImage(Context context, List<layoutImage> layoutImageList) {
+        this.mContext = context;
+        this.mLayoutImageList = layoutImageList;
     }
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v;
-        v = LayoutInflater.from(mContext).inflate(R.layout.item_layout_image,parent,false);
-        RecyclerViewAdapter_layoutImage.MyViewHolder vHolder = new RecyclerViewAdapter_layoutImage.MyViewHolder(v);
-        return vHolder;
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        view = LayoutInflater.from(mContext).inflate(R.layout.item_layout_image,parent,false);
+        RecyclerViewAdapter_layoutImage.ImageViewHolder viewHolder = new RecyclerViewAdapter_layoutImage.ImageViewHolder(view);
+        return viewHolder;
     }
 
-    public int flag = 0;
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    private final static String TAG = "Recycler_layoutImage";
 
-        holder.layoutImageView.setImageResource(mData.get(position).getCombinePicture());
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        holder.layoutImageView.setImageResource(mLayoutImageList.get(position).getCombinePicture());
         holder.combinePic.setOnClickListener(new View.OnClickListener() {
             ImageView imageView = new ImageView(mContext);
-
             public void onClick(View view) {
-                //產生ImageView object
+                Log.d(TAG, "[SelectorView] ImageView onClick, view = " + view.toString());
                 RelativeLayout relativeLayout = (RelativeLayout) view.findViewById(R.id.combineImage);
-
-                if(flag == 0){
-                    Log.d("flagXD", "flag = "+flag);
-                    //讀取圖檔
-                    imageView.setImageResource(R.drawable.circle);
-                    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(90,90);
-                    params.leftMargin = 280;
-                    params.topMargin = 280;
-                    relativeLayout.addView(imageView,params);
-                    flag = 1;
-                }else{
-                    Log.d("flagXD", "flag = "+flag);
-                    imageView.setImageResource(0);
-//                    imageView.setImageResource(android.R.color.transparent);
-
-//                    relativeLayout = (RelativeLayout) view.findViewById(R.id.combineImage);
-//                    imageView.setVisibility(View.GONE);
-//                    relativeLayout.removeView(imageView);
-
-                    //relativeLayout.removeView(imageView);
-                    flag = 0;
+                // for adding or removing the selector view.
+                if (mImageViewMap.get(view) == null) {
+                    mImageViewMap.put(view, imageView);
+                    addSelectorView(relativeLayout, imageView);
+                } else {
+                    removeSelectorView(relativeLayout, imageView);
+                    mImageViewMap.remove(view);
                 }
             }
         });
     }
 
-    @Override
-    public int getItemCount() { return mData.size(); }
+    private void addSelectorView(RelativeLayout relativeLayout, ImageView imageView) {
+        Log.d(TAG, "[SelectorView] addSelectorView, imageView = " + imageView.toString());
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        //load image resources
+        imageView.setImageResource(R.drawable.circle);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(90,90);
+        params.leftMargin = 280;
+        params.topMargin = 280;
+        try {
+            relativeLayout.addView(imageView, params);
+        } catch (Exception e) {
+            // do nothing.
+        }
+    }
+
+    private void removeSelectorView(RelativeLayout relativeLayout, ImageView imageView) {
+        Log.d(TAG, "[SelectorView] removeSelectorView, imageView = " + imageView.toString());
+        relativeLayout.removeView(imageView);
+    }
+
+    @Override
+    public int getItemCount() { return mLayoutImageList.size(); }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder{
         private ImageView layoutImageView;
         private RelativeLayout combinePic;
 
-        public MyViewHolder(@NonNull View itemView) {
+        public ImageViewHolder(@NonNull View itemView) {
             super(itemView);
 
             layoutImageView = (ImageView) itemView.findViewById(R.id.layoutImageView);
