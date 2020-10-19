51fd0c0f47982c506299e7f25137948ad821181a
Jessie Chang <jessie880405@gmail,com>
2020-10-20 02:36:55 +0800

    [PhotogifoPublicSetting] ActionBar setDisplayHome | Jessie

--- app/src/main/java/com/deep/photoeditor/activity/PhotogifPublicsetting.java
+++ app/src/main/java/com/deep/photoeditor/activity/PhotogifPublicsetting.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.deep.photoeditor.R;
+import com.deep.photoeditor.editShare;
 import com.felipecsl.gifimageview.library.GifImageView;
 
 import java.util.Arrays;
@@ -20,8 +23,12 @@ public class PhotogifPublicsetting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photogif_publicsetting);
+        //新增回到前一頁的箭頭
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        GifImageView gifView = (GifImageView) findViewById(R.id.imageView);
+        GifImageView gifView = findViewById(R.id.imageView);
+        Button btnNext = findViewById(R.id.btnNext);
 
         Intent intent=getIntent();
         if(intent !=null){
@@ -30,10 +37,14 @@ public class PhotogifPublicsetting extends AppCompatActivity {
             gifView.setBytes(fileBytes);
             gifView.startAnimation();
         }
-//        byte [] fileBytes=intent.getByteArrayExtra("GifBytes");
-//        Log.d(TAG, "fileBytes："+ Arrays.toString(fileBytes));
 
-//        gifView.setBytes(fileBytes);
-//        gifView.startAnimation();
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(PhotogifPublicsetting.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
