89e6e698de5b1da57331680e53cb27aeda92c315
vicky1000266 <vicky1000266@gmail.com>
2020-10-27 00:26:46 +0800

    my page show username api done

--- app/src/main/java/com/deep/photoeditor/fragment/PersonFragment.java
+++ app/src/main/java/com/deep/photoeditor/fragment/PersonFragment.java
@@ -2,10 +2,12 @@ package com.deep.photoeditor.fragment;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -13,8 +15,11 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.deep.photoeditor.R;
+import com.deep.photoeditor.api;
 
 public class PersonFragment extends Fragment implements View.OnClickListener {
+    //api
+    private static api callApi = new api();
     View rootView;
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Nullable
@@ -24,7 +29,19 @@ public class PersonFragment extends Fragment implements View.OnClickListener {
 
         Button btnShowCollect = (Button) rootView.findViewById(R.id.btnCollect);
         Button btnShowWorks = (Button) rootView.findViewById(R.id.btnWorks);
-
+        TextView txtPerson = rootView.findViewById(R.id.txtPerson);
+        try {
+            callApi.get("http://140.131.115.99/api/profile");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d("temp",callApi.get("http://140.131.115.99/api/profile"));
+        //留下array[]，其他切掉
+        String temp = callApi.get("http://140.131.115.99/api/profile").trim();
+        temp = temp.substring(8,(temp.length()-1));
+        Log.d("temp","cut allready :"+ temp);
+        txtPerson.setText("Hello, " + temp.replaceAll("\"",""));
+       // String person = callApi.get("http://140.131.115.99/api/profile");
         btnShowCollect.setOnClickListener(this);
         btnShowWorks.setOnClickListener(this);
 
