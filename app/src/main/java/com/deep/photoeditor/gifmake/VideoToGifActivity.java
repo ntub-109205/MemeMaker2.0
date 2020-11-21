6067b5b05b17471e93180130a0515343c84866f3
Angela822 <angelayu822@gmail.com>
2020-11-21 22:05:48 +0800

    [activity_login.xml & VideoToGifActivity.java] add hideKeyboard() | Angelayu

--- app/src/main/java/com/deep/photoeditor/gifmake/VideoToGifActivity.java
+++ app/src/main/java/com/deep/photoeditor/gifmake/VideoToGifActivity.java
@@ -2,6 +2,7 @@ package com.deep.photoeditor.gifmake;
 
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,6 +13,7 @@ import android.os.Environment;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Switch;
@@ -353,6 +355,11 @@ public class VideoToGifActivity extends GifBaseActivity implements View.OnClickL
                 super.configureChip(chip, chipConfiguration);
             }
         }, ChipSpan.class));
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 
 }
